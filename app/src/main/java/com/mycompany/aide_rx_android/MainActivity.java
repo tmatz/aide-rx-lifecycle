0ba65834c7173b3fc32dc0048221fa41de415066
MATSUDA Takashi <t.matsuda.1000@gmail.com>
2019-09-17 21:44:34 +0900

    use RxActivity

--- app/src/main/java/com/mycompany/aide_rx_android/MainActivity.java
+++ app/src/main/java/com/mycompany/aide_rx_android/MainActivity.java
@@ -1,99 +1,35 @@
 package com.mycompany.aide_rx_android;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
-import com.trello.rxlifecycle2.LifecycleProvider;
-import com.trello.rxlifecycle2.LifecycleTransformer;
-import com.trello.rxlifecycle2.RxLifecycle;
-import com.trello.rxlifecycle2.android.ActivityEvent;
-import com.trello.rxlifecycle2.android.RxLifecycleAndroid;
-import io.reactivex.Observable;
+import com.trello.rxlifecycle2.components.RxActivity;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.functions.Consumer;
-import io.reactivex.subjects.BehaviorSubject;
 
-public class MainActivity extends Activity
-implements LifecycleProvider<ActivityEvent>
+public class MainActivity extends RxActivity
 {
-    private final BehaviorSubject<ActivityEvent> mLifecycle = BehaviorSubject.create();
-
-    @Override
-    public final Observable<ActivityEvent> lifecycle()
-    {
-        return mLifecycle.hide();
-    }
-
-    @Override
-    public final <T> LifecycleTransformer<T> bindUntilEvent(ActivityEvent event)
-    {
-        return RxLifecycle.bindUntilEvent(lifecycle(), event);
-    }
-
-    @Override
-    public final <T> LifecycleTransformer<T> bindToLifecycle()
-    {
-        return RxLifecycleAndroid.bindActivity(lifecycle());
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
-        mLifecycle.onNext(ActivityEvent.CREATE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
         final MainViewModel vm = new MainViewModel();
+
         vm.dateTime
             .compose(bindToLifecycle())
-            .cast(String.class)
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(bind((TextView)findViewById(R.id.clock)));
     }
 
-    @Override
-    protected void onStart()
+    private static Consumer<Object> bind(final TextView textView)
     {
-        mLifecycle.onNext(ActivityEvent.START);
-        super.onStart();
-    }
-
-    @Override
-    protected void onResume()
-    {
-        mLifecycle.onNext(ActivityEvent.PAUSE);
-        super.onResume();
-    }
-
-    @Override
-    protected void onPause()
-    {
-        mLifecycle.onNext(ActivityEvent.PAUSE);
-        super.onPause();
-    }
-
-    @Override
-    protected void onStop()
-    {
-        mLifecycle.onNext(ActivityEvent.STOP);
-        super.onStop();
-    }
-
-    @Override
-    protected void onDestroy()
-    {
-        mLifecycle.onNext(ActivityEvent.DESTROY);
-        super.onDestroy();
-    }
-
-    private static Consumer<String> bind(final TextView textView)
-    {
-        return new Consumer<String>()
+        return new Consumer<Object>()
         {
             @Override
-            public void accept(String v) throws Exception
+            public void accept(Object value) throws Exception
             {
-                textView.setText(v.toString());
+                textView.setText(value.toString());
             }
         };
     }
